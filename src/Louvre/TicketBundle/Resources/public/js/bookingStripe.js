3b96a04ab32c2237f30d287fe677a7cb6f06ee2a
ChuGui <gchurlet@gmail.com>
2017-10-17 13:19:48 +0200

    Grosse MAJ en utilisant composer update --ignore-platform-reqs. Message d'erreur: [RuntimeException]

--- src/Louvre/TicketBundle/Resources/public/js/bookingStripe.js
+++ src/Louvre/TicketBundle/Resources/public/js/bookingStripe.js
@@ -1,6 +1,10 @@
 var stripe = Stripe('pk_test_lvefCuTVu1SQoYPdJO57yFYv');
 var $form = $('#payment_form');
 var elements = stripe.elements();
+$(document).ready(function(){
+    $("#paiementBalise").addClass('active');
+});
+
 
 $form.submit(function (e)
 {
---
+++ src/Louvre/TicketBundle/Tests/CalculatePriceTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class CalculatePriceTest extends PHPUnit_Framewwork_TestCase
+{
+
+    public function testCalulatePrice()
+    {
+
+    }
+
+}
